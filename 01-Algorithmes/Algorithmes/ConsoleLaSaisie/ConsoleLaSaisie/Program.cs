580ec4845deda2c110cacaef6067dc64fdcd3932
Jéjé <36699327+vassili54@users.noreply.github.com>
2024-09-25 16:36:15 +0200

    Contrôler la saisie

---
+++ 01-Algorithmes/Algorithmes/ConsoleLaSaisie/ConsoleLaSaisie/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+L’utilisateur est invité à saisir son prénom.
+Le programme affiche ensuite “Bonjour” suivi du prénom saisi.
+Le prénom doit contenir au moins 2 caractères.
+Si le prénom contient moins de 2 caractères, l’utilisateur doit recommencer la saisie.
+L’utilisateur dispose d’un nombre d’essai illimité. 
+*/
+ 
+
+
+//VARIABLES
+
+string prenom;
+
+//TRAITEMENT
+
+do
+{
+    Console.Write("Veuillez saisir votre prénom (au moins 2 caractères) : ");
+    prenom = Console.ReadLine();
+} while (prenom.Length < 2);
+
+//AFFICHAGE
+
+Console.WriteLine("Bonjour " + prenom + " !");
